650650eb1258c5ff6376d2696d6cfe2c45128cb5
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-03-03 09:47:09 +0100

    other links test

---
+++ src/services/extraction/otherLinkParser.test.js
@@ -0,0 +1,22 @@
+const { getContent } = require('../fileServices/readFile');
+const { parseLinks } = require('./otherLinkParser');
+
+const linksEffectsFilePathForCard =
+  cardNum => `./src/services/extraction/test-data/linksEffects${cardNum}.html`;
+
+
+describe('parse other links', () => {
+  it('parse optional Effects card 1', async () => {
+    const content = await getContent(linksEffectsFilePathForCard(1));
+    const links = parseLinks(content);
+    expect(links).toMatchSnapshot();
+    expect(links.length).toBe(2);
+    expect(links[0].explanation).toContain("L'être humain occupe presque");
+    expect(links[0].href).toEqual("/wiki/index.php?title=Fr-fr_adulte_carte_25_biodiversit%C3%A9_terrestre");
+});
+it('parse optional Effects card 20', async () => {
+  const content = await getContent(linksEffectsFilePathForCard(20));
+  const links = parseLinks(content);
+  expect(links).toMatchSnapshot();
+});
+});
