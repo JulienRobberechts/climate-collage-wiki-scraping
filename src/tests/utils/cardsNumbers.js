8f2aa3b2b0ecc8ea93bc69c4c87ab6ad78b0bca3
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-03-28 22:29:09 +0200

    card 18 is fixed

--- src/tests/utils/cardsNumbers.js
+++ src/tests/utils/cardsNumbers.js
@@ -1,7 +1,6 @@
 
-// to remove when the page 18 will be fixed
-// https://fresqueduclimat.org/wiki/index.php?title=Fr-fr_adulte_carte_18_fonte_de_la_banquise
-const knownErrors = [18];
+// To add when there are bugs on some cards
+const knownErrors = [];
 
 const oneTo42 = [...Array(41).keys()].map(n => n + 1).filter(n => !knownErrors.find(x => x === n));
 const cards1To10 = [...Array(10).keys()].map(n => n + 1);
