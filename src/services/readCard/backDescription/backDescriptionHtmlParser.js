886c2d0e5598d0021e9af3e1e4c0a79ed1798f74
Julien Robberechts <julien.robberechts-ext@socgen.com>
2021-04-06 15:59:13 +0200

    error display

--- src/services/readCard/backDescription/backDescriptionHtmlParser.js
+++ src/services/readCard/backDescription/backDescriptionHtmlParser.js
@@ -5,7 +5,7 @@ const { assertEqual, assertMore } = require('../../utils/assert/parserAssertions
 module.exports.parseBackDescription = (content, message = '') => {
   const { window: { document } } = new JSDOM(content);
   const paragraphes = document.querySelectorAll("table ~ p");
-  assertMore('paragraphes count' + message, paragraphes.length, 1);
+  assertMore('paragraphes count - ' + message, paragraphes.length, 1);
   const backDescription = Array.from(paragraphes).reduce((text, p) => text + p.textContent.trim(), '')
   return backDescription;
 };
