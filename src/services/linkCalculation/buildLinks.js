873e43e48f4acced25e0dcf90bfc3ebe90800bd6
Julien Robberechts <robberechts.consutling@gmail.com>
2021-10-30 12:32:00 +0200

    remove useless code

--- src/services/linkCalculation/buildLinks.js
+++ src/services/linkCalculation/buildLinks.js
@@ -1,23 +1,16 @@
+const { getConfig } = require("../../config/config");
 
-const { getConfig } = require('../../config/config');
-
-const getCardByNum = (cards, cardNumber) => {
-  const card = cards.find(c => c.cardNum.toString() === cardNumber.toString());
-  if (!card)
-    throw new Error(`Card not found with number ${cardNumber}`);
-  return card;
-};
-
-const getCardNumberFromUrl = (cardUrl, lang = 'fr') => {
+const getCardNumberFromUrl = (cardUrl, lang = "fr") => {
   const { cardUrlRegexp } = getConfig(lang);
   const found = cardUrlRegexp.exec(cardUrl);
   if (!found) {
-    throw new Error(`impossible to find the card number in this url: ${cardUrl}`);
+    throw new Error(
+      `impossible to find the card number in this url: ${cardUrl}`
+    );
     // return -1;
   }
-  const cardNumber = found.groups['num'];
+  const cardNumber = found.groups["num"];
   return parseInt(cardNumber, 10);
 };
-
 
 module.exports = { getCardNumberFromUrl };
