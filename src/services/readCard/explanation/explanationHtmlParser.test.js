eaaec94b9df9abe626209cf601b15149cfc50d11
Julien Robberechts <robberechts.consutling@gmail.com>
2021-10-30 12:50:53 +0200

    clean up tests files

--- src/services/readCard/explanation/explanationHtmlParser.test.js
+++ src/services/readCard/explanation/explanationHtmlParser.test.js
@@ -1,11 +1,12 @@
-const { getContent } = require('../../utils/fileServices/readFile');
-const { parseExplanation } = require('./explanationHtmlParser');
+const { getContent } = require("../../utils/fileServices/readFile");
+const { parseExplanation } = require("./explanationHtmlParser");
 
 // to update those files check manual-calls/get -card-sections.http
-const cardContent1Section1FilePath = './src/tests/data/fr/carte1-1.html';
+const cardContent1Section1FilePath =
+  "./src/tests/data/fr/carte1-Explications.html";
 
-describe('parse Explanation', () => {
-  it('parse explanation card 1', async () => {
+describe("parse Explanation", () => {
+  it("parse explanation card 1", async () => {
     const content = await getContent(cardContent1Section1FilePath);
     const result = parseExplanation(content);
     expect(result).toMatchSnapshot();
