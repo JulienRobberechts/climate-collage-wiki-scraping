01372ed758dce46ce7288625748222874baefafa
Julien Robberechts <robberechts.consutling@gmail.com>
2021-10-30 18:37:24 +0200

    activate EN links tests

--- src/tests/on-results/en/links.test.js
+++ src/tests/on-results/en/links.test.js
@@ -4,8 +4,7 @@ const lang = "en";
 const langRef = "fr";
 
 describe("Links", () => {
-  // FR and EN version are not synchronized at the moment (30/10/2021)
-  it.skip("Missing links in English version (compared to FR)", async () => {
+  it("Missing links in English version (compared to FR)", async () => {
     await checkMissingLinks(lang, langRef);
   });
 });
--- src/tests/on-results/fr/links.test.js
+++ src/tests/on-results/fr/links.test.js
@@ -4,8 +4,7 @@ const lang = "fr";
 const langRef = "en";
 
 describe("Links", () => {
-  // FR and EN version are not synchronized at the moment (30/10/2021)
-  it.skip("Missing links in French version (compared to EN)", async () => {
+  it("Missing links in French version (compared to EN)", async () => {
     await checkMissingLinks(lang, langRef);
   });
 });
