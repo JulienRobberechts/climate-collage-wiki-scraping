fa47490ae6bbb59acd6ab10d7d43580f2c708828
Julien Robberechts <robberechts.consutling@gmail.com>
2021-11-13 04:13:45 +0100

    fix displaystyle

--- src/services/utils/string/cleanUpString.js
+++ src/services/utils/string/cleanUpString.js
@@ -23,11 +23,16 @@ const removeSemiColumn = (input) => {
 
 const cleanUpStringSpecific = (input) => {
   return cleanUpStringBasic(input)
+    .replace("10 15 {\\displaystyle 10^{15}}", "10^15")
+    .replace(" 10 21 {\\displaystyle 10^{21}", "10^21")
+    .replace("3 , 8 W / m 2 {\\displaystyle 3,8W/m^{2}} ", "3,8 W/m^2")
+    .replace("2.8 W / m 2 {\\displaystyle 2.8W/m^{2}} ", "2,8 W/m^2")
     .replace("3 , 1 W / m 2 {\\displaystyle 3,1W/m^{2}}", "3,1 W/m2")
+    .replace("3 , 1 W / m 2 {\\displaystyle 3,1W/m^{2}}", "3,1 W/m^2")
     .replace("m 2 {\\displaystyle m^{2}} ", "métre carré")
-    .replace("0 , 8 W / m 2 {\\displaystyle -0,8W/m^{2}}", "0,8 W/m2")
-    .replace("2 , 3 W / m 2 {\\displaystyle 2,3W/m^{2}}", "2,3 W/m2")
-    .replace("2 , 6 W / m 2 {\\displaystyle 2,6W/m^{2}}", "2,6 W/m2");
+    .replace("0 , 8 W / m 2 {\\displaystyle -0,8W/m^{2}}", "0,8 W/m^2")
+    .replace("2 , 3 W / m 2 {\\displaystyle 2,3W/m^{2}}", "2,3 W/m^2")
+    .replace("2 , 6 W / m 2 {\\displaystyle 2,6W/m^{2}}", "2,6 W/m^2");
 };
 
 module.exports = { cleanUpStringBasic, cleanUpStringSpecific };
