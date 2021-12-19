f8d2a9448da9562e1029382f0d58de51599a5b73
Julien Robberechts <robberechts.consutling@gmail.com>
2021-12-19 18:14:34 +0100

    simplify test

--- src/services/readCard/explanation/explanation.test.js
+++ src/services/readCard/explanation/explanation.test.js
@@ -1,47 +1,22 @@
 /**
  * @jest-environment node
  */
-const { getExplanation } = require('./explanation');
-const { getCardsFrReferenceByCardNum } = require('../../data-access/cardsRepo');
-const { oneTo42 } = require('../../../tests/utils/cardsNumbers');
+const { getExplanation } = require("./explanation");
+const { getCardsFrReferenceByCardNum } = require("../../data-access/cardsRepo");
+const { oneTo42 } = require("../../../tests/utils/cardsNumbers");
 
-const lang = 'fr';
+const lang = "fr";
 
-describe('explanation', () => {
-  it.each(oneTo42)('check explanation on card %i', async (cardNum) => {
+describe("explanation", () => {
+  it.each(oneTo42)("check explanation on card %i", async (cardNum) => {
     const card = await getCardsFrReferenceByCardNum(cardNum);
     const explanation = await getExplanation(card.wikiId, lang);
     expect(explanation).toStrictEqual(card.explanation);
   });
-  it('check explanation on card 1', async () => {
-    const cardNum = 1;
+  it("check explanation on card 21*", async () => {
+    const cardNum = 21;
     const card = await getCardsFrReferenceByCardNum(cardNum);
     const explanation = await getExplanation(card.wikiId, lang);
     expect(explanation).toStrictEqual(card.explanation);
   }, 30000);
-  it('check explanation on card 3', async () => {
-    const cardNum = 3;
-    const card = await getCardsFrReferenceByCardNum(cardNum);
-    const explanation = await getExplanation(card.wikiId, lang);
-    expect(explanation).toStrictEqual(card.explanation);
-  }, 30000);
-  it('check explanation on card 6', async () => {
-    const cardNum = 6;
-    const card = await getCardsFrReferenceByCardNum(cardNum);
-    const explanation = await getExplanation(card.wikiId, lang);
-    expect(explanation).toStrictEqual(card.explanation);
-  }, 30000);
-
-  it('check explanation on card 9', async () => {
-    const cardNum = 9;
-    const card = await getCardsFrReferenceByCardNum(cardNum);
-    const explanation = await getExplanation(card.wikiId, lang);
-    expect(explanation).toStrictEqual(card.explanation);
-  });
-  it('check explanation on card 13', async () => {
-    const cardNum = 13;
-    const card = await getCardsFrReferenceByCardNum(cardNum);
-    const explanation = await getExplanation(card.wikiId, lang);
-    expect(explanation).toStrictEqual(card.explanation);
-  });
 });
